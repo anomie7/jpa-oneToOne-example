986529a4f57b9ecfeb4b8e3c0f8e15d554f1952e
minu33 <anomie7777@gmail.com>
2020-05-02 19:37:37 +0900

    init

---
+++ src/test/kotlin/com/example/jparelationship/demo/JpaOneToOneRelationshipExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jparelationship.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaOneToOneRelationshipExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
