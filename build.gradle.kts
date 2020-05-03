13af21067daeab4c335c21d1a13e347115a7be7d
minu33 <anomie7777@gmail.com>
2020-05-03 19:43:25 +0900

    mysql에서 로그를 보기 위해 datasource를 mysql로 설정 의존성에 mysql connector 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 }
 
 dependencies {
+	implementation("mysql:mysql-connector-java:8.0.14")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
--- src/main/resources/data-h2-test.sql
+++ src/main/resources/data-h2-test.sql
@@ -3,3 +3,9 @@ insert into post (id, title, details_id) values (1, 'JPA에서 일대일 연관 
 
 insert into post_details (id, content) values (2, '안녕하세요. 여러분 게시물 내용2')
 insert into post (id, title, details_id) values (2, 'JPA에서 일대일 연관 관계 정의시 고려할 점2', 2)
+
+insert into post_details (id, content) values (3, '안녕하세요. 여러분 게시물 내용3')
+insert into post (id, title, details_id) values (3, 'JPA에서 일대일 연관 관계 정의시 고려할 점3', 3)
+
+insert into post_details (id, content) values (4, '안녕하세요. 여러분 게시물 내용4')
+insert into post (id, title, details_id) values (4, 'JPA에서 일대일 연관 관계 정의시 고려할 점4', 4)
